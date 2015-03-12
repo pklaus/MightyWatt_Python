7e3b4c04cdca5f2f03e0ee98724add132f3ebb32
Philipp Klaus <philipp.klaus@gmail.com>
2015-03-12 21:38:04 +0100

    initial version

---
+++ mw.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+import serial
+import struct
+import time
+from pprint import pprint
+
+class MightyWatt(object):
+
+    LE = "\r\n"            # line ending
+    IDN_q = b"\x1F"        # identify query byte
+    IDN_r = "Mighty Watt"  # expected identify answer
+    QDC_q = b"\x1E"        # query command byte
+    QDC_r = [              # meaning of the answer lines
+        ('FW_VERSION', str),
+        ('BOARD_REVISION', str),
+        ('maxIdac', lambda x: int(x)/1000.),
+        ('maxIadc', lambda x: int(x)/1000.),
+        ('maxVdac', lambda x: int(x)/1000.),
+        ('maxVadc', lambda x: int(x)/1000.),
+        ('MAX_POWER', lambda p: int(p)/1000.),
+        ('DVM_INPUT_RESISTANCE', int),
+        ('temperatureThreshold', int),
+        ('MAX_TEMPERATURE', int),
+        ('MIN_TEMPERATURE', int),
+    ]
+    STATUS = {
+        0: 'READY',
+        1: 'CURRENT_OVERLOAD',
+        2: 'VOLTAGE_OVERLOAD',
+        4: 'POWER_OVERLOAD',
+        8: 'OVERHEAT'
+    }
+    UPD_fmt = '>HHBBBB'
+    UPD_r = [              # update response
+        ('current', lambda i: i/1000.),
+        ('voltage', lambda v: v/1000.),
+        ('temperature', int),
+        ('temperatureThreshold', int),
+        ('remoteStatus', lambda rs: bool(rs & 0x01)),
+        ('currentStatus', lambda x: x)
+    ]
+    MODE_CC =   0 | 0x80 | (2 << 5)
+    MODE_CV =   1 | 0x80 | (2 << 5)
+    MODE_CP =   2 | 0x80 | (3 << 5)
+    MODE_CR =   3 | 0x80 | (3 << 5)
+    TEMPERATURE_THRESHOLD_ID = 4 | 0x80 | (1 << 5)
+    REMOTE_ID = 5 | 0x80 | (1 << 5)
+
+    def __init__(self, port, verbose=False):
+        self.port = port
+        self.verbose = verbose
+        self._connect()
+        if self.verbose: self.print_device_summary()
+
+    def _connect(self):
+        self._c = serial.Serial(self.port, 115200, timeout=0)
+        #time.sleep(0.001)
+        self._identify()
+        self._read_properties()
+
+    def _identify(self):
+        self._c.write(MightyWatt.IDN_q)
+        time.sleep(0.02)
+        assert self._c.readline().decode('ascii') == MightyWatt.IDN_r + MightyWatt.LE
+
+    def _read_properties(self):
+        self._c.write(MightyWatt.QDC_q)
+        time.sleep(0.02)
+        response = self._c.read(200).decode('ascii')
+        response = response.strip()
+        response = response.split(MightyWatt.LE)
+        assert len(response) == len(MightyWatt.QDC_r)
+        self.props = {}
+        for i in range(len(MightyWatt.QDC_r)):
+            value = MightyWatt.QDC_r[i][1](response[i])
+            self.props[MightyWatt.QDC_r[i][0]] = value
+
+    def print_device_summary(self):
+        print("Connected to a MightyWatt on port {}".format(self.port))
+        print("Properties:")
+        pprint(self.props)
+
+    def update(self):
+        self._c.write(b"\x8F")
+        time.sleep(0.02)
+        response = self._c.read(struct.calcsize(MightyWatt.UPD_fmt))
+        self._set_status(response)
+
+    def set_cc(self, current):
+        current = int(round(current*1000.))
+        self._c.write(struct.pack('>BH', MightyWatt.MODE_CC, current))
+        time.sleep(0.02)
+        response = self._c.read(struct.calcsize(MightyWatt.UPD_fmt))
+        self._set_status(response)
+
+    def _set_status(self, response):
+        response = struct.unpack(MightyWatt.UPD_fmt, response)
+        status = dict()
+        for i in range(len(MightyWatt.UPD_r)):
+            value = MightyWatt.UPD_r[i][1](response[i])
+            status[MightyWatt.UPD_r[i][0]] = value
+        self.status = status
+
+    def print_status(self):
+        pprint(self.status)
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='Mighty Watt tool')
+    parser.add_argument('serial_port', help='The serial port to connect to')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Generate verbose output')
+    args = parser.parse_args()
+    mw = MightyWatt(args.serial_port, verbose=args.verbose)
+    mw.update()
+    mw.print_status()
+    mw.set_cc(1.00)
+    mw.print_status()
+    time.sleep(0.5)
+    mw.update()
+    mw.print_status()
+
+if __name__ == "__main__":
+    main()
