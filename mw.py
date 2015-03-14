500ca24cb34d2af648dd7a9cb9acb70e10f0a951
Philipp Klaus <philipp.klaus@gmail.com>
2015-03-14 13:05:33 +0100

    custom Exceptions / retry connect()
    * Custom exceptions are now implemented for easier error checking.
      Wrappers for pySerial's read(), write() and readline() are used now.
    * If the initial IDN / QDC queries will fail, they will now retry.
      This was needed when the device wasn't quite ready yet directly
      after plugging it in.

--- mw.py
+++ mw.py
@@ -49,36 +49,88 @@ class MightyWatt(object):
     TEMPERATURE_THRESHOLD_ID = 4 | 0x80 | (1 << 5)
     REMOTE_ID = 5 | 0x80 | (1 << 5)
 
+    update_rate = 5
+    props = None
+    message_size = 0
+    identity = None
+    idn_tries = 20
+    qdc_tries = 20
+
     def __init__(self, port, verbose=False):
         self.port = port
         self.verbose = verbose
+        self.message_size = struct.calcsize(MightyWatt.UPD_fmt)
         self._connect()
         if self.verbose: self.print_device_summary()
-        self.timer = PerpetualTimer(0.2, self.update)
+        self.timer = PerpetualTimer(1/self.update_rate, self.update)
         self.timer.start()
 
     def _connect(self):
-        self._c = serial.Serial(self.port, 115200, timeout=0)
+        try:
+            self._c = serial.Serial(self.port, 115200, timeout=0.1)
+        except (serial.serialutil.SerialException, FileNotFoundError):
+            raise MightyWattCommunicationException()
         #time.sleep(0.001)
         self._identify()
         self._read_properties()
+        self.update()
 
     def _identify(self):
-        self._c.write(MightyWatt.IDN_q)
-        time.sleep(0.02)
-        assert self._c.readline().decode('ascii') == MightyWatt.IDN_r + MightyWatt.LE
+        while not self.identity and self.idn_tries > 0:
+            answer = ''
+            try:
+                self._write(MightyWatt.IDN_q)
+                time.sleep(0.02)
+                answer = self._readline()
+                assert answer == MightyWatt.IDN_r + MightyWatt.LE, "strange answer: " + answer
+                self.identity = answer.strip()
+                return
+            except AssertionError:
+                if self.verbose: print('Not a valid IDN response: ' + str(answer))
+                self.idn_tries -= 1
+                time.sleep(0.1)
+        raise MightyWattCommunicationException("Didn't get a proper IDN response.")
+
+    def _write(self, *args, **kwargs):
+        try:
+            return self._c.write(*args, **kwargs)
+        except (OSError, serial.serialutil.SerialException):
+            raise MightyWattCommunicationException()
+
+    def _read(self, *args, **kwargs):
+        try:
+            return self._c.read(*args, **kwargs)
+        except (OSError, serial.serialutil.SerialException):
+            raise MightyWattCommunicationException()
+
+    def _readline(self):
+        try:
+            return self._c.readline().decode('ascii')
+        except (OSError, serial.serialutil.SerialException):
+            raise MightyWattCommunicationException()
 
     def _read_properties(self):
-        self._c.write(MightyWatt.QDC_q)
-        time.sleep(0.02)
-        response = self._c.read(200).decode('ascii')
-        response = response.strip()
-        response = response.split(MightyWatt.LE)
-        assert len(response) == len(MightyWatt.QDC_r)
-        self.props = {}
-        for i in range(len(MightyWatt.QDC_r)):
-            value = MightyWatt.QDC_r[i][1](response[i])
-            self.props[MightyWatt.QDC_r[i][0]] = value
+        while not self.props and self.qdc_tries > 0:
+            response = []
+            try:
+                self._write(MightyWatt.QDC_q)
+                time.sleep(0.02)
+                #response = self._read(200).decode('ascii')
+                #response = response.strip()
+                #response = response.split(MightyWatt.LE)
+                response = [self._readline() for i in range(len(MightyWatt.QDC_r))]
+                assert len(response) == len(MightyWatt.QDC_r)
+                props = {}
+                for i in range(len(MightyWatt.QDC_r)):
+                    value = MightyWatt.QDC_r[i][1](response[i])
+                    props[MightyWatt.QDC_r[i][0]] = value
+                self.props = props
+                return
+            except (AssertionError, ValueError):
+                if self.verbose: print("Not a valid QDC response: " + str(response))
+                self.qdc_tries -= 1
+                time.sleep(0.1)
+        raise MightyWattCommunicationException("Didn't get a proper QDC response.")
 
     def print_device_summary(self):
         print("Connected to a MightyWatt on port {}".format(self.port))
@@ -86,38 +138,47 @@ class MightyWatt(object):
         pprint(self.props)
 
     def update(self):
-        self._c.write(b"\x8F")
+        self._write(b"\x8F")
         self._update_status()
+
+    def set_update_rate(self, rate):
+        assert rate < 30 and rate > 1.2
+        self.timer.wait_time = 1/self.update_rate
 
     def _update_status(self):
         time.sleep(0.02)
-        response = self._c.read(struct.calcsize(MightyWatt.UPD_fmt))
+        try:
+            response = self._read(self.message_size)
+        except (OSError, serial.serialutil.SerialException):
+            raise MightyWattCommunicationException()
+        if len(response) != self.message_size:
+            raise MightyWattCommunicationException()
         self._set_status(response)
 
     def set_cc(self, current):
         current = int(round(current*1000.))
-        self._c.write(struct.pack('>BH', MightyWatt.MODE_CC, current))
+        self._write(struct.pack('>BH', MightyWatt.MODE_CC, current))
         self._update_status()
 
     def set_cv(self, voltage):
         voltage = int(round(voltage*1000.))
-        self._c.write(struct.pack('>BH', MightyWatt.MODE_CV, voltage))
+        self._write(struct.pack('>BH', MightyWatt.MODE_CV, voltage))
         self._update_status()
 
     def set_cp(self, power):
         power = int(round(power*1000.))
         power = three_bytes(power)
-        self._c.write(struct.pack('>BBBB', MightyWatt.MODE_CP, *power))
+        self._write(struct.pack('>BBBB', MightyWatt.MODE_CP, *power))
         self._update_status()
 
     def set_cr(self, resistance):
         resistance = int(round(resistance*1000.))
         resistance = three_bytes(resistance)
-        self._c.write(struct.pack('>BBBB', MightyWatt.MODE_CR, *resistance))
+        self._write(struct.pack('>BBBB', MightyWatt.MODE_CR, *resistance))
         self._update_status()
 
     def set_remote(self, remote=True):
-        self._c.write(struct.pack('>BB', MightyWatt.REMOTE_ID, int(remote)))
+        self._write(struct.pack('>BB', MightyWatt.REMOTE_ID, int(remote)))
         self._update_status()
 
     def set_local(self, local=True):
@@ -151,6 +212,12 @@ class MightyWatt(object):
     def __del__(self):
         self.close()
 
+class MightyWattException(Exception):
+    pass
+
+class MightyWattCommunicationException(MightyWattException):
+    pass
+
 class PerpetualTimer(threading.Thread):
     def __init__(self, wait_time, func, *args):
         self.wait_time = wait_time
@@ -162,7 +229,7 @@ class PerpetualTimer(threading.Thread):
     def run(self):
         while not self.event.is_set():
             self.func(*self.args)
-            self.event.wait(0.2)
+            self.event.wait(self.wait_time)
 
     def stop(self):
         self.event.set()
@@ -176,41 +243,50 @@ def main():
     parser.add_argument('serial_port', help='The serial port to connect to')
     parser.add_argument('--verbose', '-v', action='store_true', help='Generate verbose output')
     args = parser.parse_args()
-    mw = MightyWatt(args.serial_port, verbose=args.verbose)
-    mw.update()
-    mw.print_status()
-    mw.set_cc(1.00)
-    mw.print_status()
-    time.sleep(0.5)
-    mw.set_cc(1.00)
-    mw.print_status()
-    time.sleep(0.5)
-    mw.update()
-    mw.print_status()
-    #mw.set_remote()
-    mw.print_status()
-    time.sleep(0.5)
-    mw.update()
-    mw.print_status()
-    mw.set_cp(2.5)
-    mw.print_status()
-    time.sleep(0.7)
-    mw.update()
-    mw.print_status()
-    time.sleep(2.5)
-    mw.update()
-    mw.print_status()
-    mw.set_cv(5.1)
-    mw.print_status()
-    time.sleep(0.7)
-    mw.set_cv(5.1)
-    time.sleep(0.7)
-    mw.update()
-    mw.print_status()
-    time.sleep(2.5)
-    mw.update()
-    mw.print_status()
-    mw.close()
+    try:
+        try:
+            mw = MightyWatt(args.serial_port, verbose=args.verbose)
+        except MightyWattCommunicationException:
+            parser.error('An error occured, could not establish a connection to the device.')
+        mw.update()
+        mw.print_status()
+        mw.set_cc(1.00)
+        mw.print_status()
+        time.sleep(0.5)
+        mw.set_cc(1.00)
+        mw.print_status()
+        time.sleep(0.5)
+        mw.update()
+        mw.print_status()
+        #mw.set_remote()
+        mw.print_status()
+        time.sleep(0.5)
+        mw.update()
+        mw.print_status()
+        mw.set_cp(2.5)
+        mw.print_status()
+        time.sleep(0.7)
+        mw.update()
+        mw.print_status()
+        time.sleep(2.5)
+        mw.update()
+        mw.print_status()
+        mw.set_cv(5.1)
+        mw.print_status()
+        time.sleep(0.7)
+        mw.set_cv(5.1)
+        time.sleep(0.7)
+        mw.update()
+        mw.print_status()
+        time.sleep(2.5)
+        mw.update()
+        mw.print_status()
+        mw.close()
+    except KeyboardInterrupt:
+        try:
+            mw.close()
+        except:
+            pass
 
 if __name__ == "__main__":
     main()
